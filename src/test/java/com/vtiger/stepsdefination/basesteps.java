3977d16678a9aec24e652b57b5c69b6ad51832b0
anjali10tidke <anjalitidke.888@gmail.com>
2025-03-11 16:59:12 +0530

    implemented datatable

---
+++ src/test/java/com/vtiger/stepsdefination/basesteps.java
@@ -0,0 +1,14 @@
+package com.vtiger.stepsdefination;
+
+import com.vtiger.pages.HomePage;
+import com.vtiger.pages.LeadPage;
+import com.vtiger.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+
+public class basesteps
+{
+    public static WebDriver driver;
+    public static LoginPage lp;
+    public static HomePage hp;
+    public static LeadPage ldp;
+}
